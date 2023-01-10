69d55ae2e3093ca91a91d6435c280903a4363fe9
BandguyMichael <leibmi1@furman.edu>
2023-01-10 15:13:43 -0500

    a

--- src/utilities/ArraySet.java
+++ src/utilities/ArraySet.java
@@ -89,9 +89,8 @@ public class ArraySet<E> implements List<E>, Set<E>
 	}
 
 	@Override
-	public int lastIndexOf(Object arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int lastIndexOf(Object element) {
+		
 	}
 
 	@Override
@@ -108,56 +107,47 @@ public class ArraySet<E> implements List<E>, Set<E>
 
 	@Override
 	public boolean remove(Object arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		
 	}
 
 	@Override
 	public E remove(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 	@Override
 	public boolean removeAll(Collection<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		
 	}
 
 	@Override
 	public boolean retainAll(Collection<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
+		
 	}
 
 	@Override
 	public E set(int arg0, E arg1) {
-		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		
 	}
 
 	@Override
 	public List<E> subList(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 	@Override
 	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 	@Override
 	public <T> T[] toArray(T[] arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 }
