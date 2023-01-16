9f2ef7c439709c598ae8b61100e3399904d95e5e
Michael Thomas <michaelhthomas@outlook.com>
2023-01-16 17:04:48 -0500

    implement testRetainAll

--- test/utilities/ArraySetTest.java
+++ test/utilities/ArraySetTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -59,12 +60,28 @@ class ArraySetTest
 
 	@Test
 	void testAddAllCollectionOfQextendsE() {
+		ArraySet<String> test = new ArraySet<>();
+
+		test.addAll(Arrays.asList(testItems));
 		
+		// make sure duplicate keys are removed
+		assertEquals(testItemsSet.length, test.size());
+
+		// make sure the lists are equal
+		assertArrayEquals(testItemsSet, test._list.toArray());
 	}
 
 	@Test
 	void testRetainAll() {
+		ArraySet<String> test = new ArraySet<>(Arrays.asList(testItems));
+
+		List<String> retainItems = List.of("shark", "whale", "dog");
 		
+		test.retainAll(retainItems);
+
+		assertEquals(3, test.size());
+
+		assertEquals(retainItems, test._list);
 	}
 
 	@Test
