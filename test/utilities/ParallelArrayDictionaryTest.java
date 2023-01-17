187d2724e4b522531ccefc306024459ed6216303
BandguyMichael <leibmi1@furman.edu>
2023-01-17 14:41:23 -0500

    Update ParallelArrayDictionaryTest.java

--- test/utilities/ParallelArrayDictionaryTest.java
+++ test/utilities/ParallelArrayDictionaryTest.java
@@ -1,21 +1,39 @@
 package utilities;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class ParallelArrayDictionaryTest
 {
 	@Test
 	void testParallelArrayDictionary() {
-		ParallelArrayDictionary dingus = new ParallelArrayDictionary();
+		ParallelArrayDictionary<Integer, Integer> dingus = new ParallelArrayDictionary<>();
+		
+		ParallelArrayDictionary<Double, String> ParallelArrayDictionary = new ParallelArrayDictionary<>();
+		
+		ParallelArrayDictionary<Object, Float> status = new ParallelArrayDictionary<>();
+		
+		ParallelArrayDictionary<Integer, Long> dingalingadingdong = new ParallelArrayDictionary<>();
+	}
+
+	ParallelArrayDictionary<Integer, String> pad = new ParallelArrayDictionary<>();
+	
+	@Test
+	void testPut() {
+		assertEquals("github took a dump today", pad.put(1,  "github took a dump today"));
+		
+		assertEquals("Dr. Alvin", pad.put(2,  "Dr. Alvin"));
+		
+		assertEquals("igloo", pad.put(3,  "igloo"));
+		
+		assertEquals("alienware", pad.put(4, "alienware"));
+		
+		assertEquals("test", pad.put(1,  "test"));
 	}
 
 	@Test
 	void testGet() {
-		
-	}
-
-	@Test
-	void testPut() {
 		
 	}
 
